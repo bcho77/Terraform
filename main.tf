f2dc06c6300eee04cb205939c65916afa98f2c24
bcho77 <temgvan@gmail.com>
2024-01-16 23:20:52 +0100

    creating an ec2 amazon

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example"  {
-  ami           = "ami-0c2f3d2ee24929520"  # Replace with your desired AMI ID
+  ami           = "ami-0005e0cfe09cc9050"  # Replace with your desired AMI ID
   instance_type = "t2.micro"                # Replace with your desired instance type
 
   tags = {
