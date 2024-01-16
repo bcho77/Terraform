c40e6d1c2359d14dfad884d1a9325b60a50443b3
bcho77 <temgvan@gmail.com>
2024-01-16 22:57:32 +0100

    creating an ec2 amazon

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "us-east-2"  # Replace with your desired AWS region
-  access_key = "AKIA4MUKYET6QETUAEOX"
-  secret_key = "IOcDK24lA5yXRnc16jtlrdlqSl5xw6UuWq0LLlf+"
 }
 
 resource "aws_instance" "example"  {
