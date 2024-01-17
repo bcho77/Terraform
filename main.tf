abeba0afdc86805011454263bb28554445f66ee1
bcho77 <tengvan@gmail.com>
2024-01-17 19:56:30 +0000

    adding tree instances

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
-provider "aws" {
-  region = "us-east-2"  # Replace with your desired AWS region
-}
-
 resource "aws_instance" "example"  {
+
+  count = 3
   ami           = "ami-05803413c51f242b7"  # Replace with your desired AMI ID
-  instance_type = "t2.micro"                # Replace with your desired instance type
+  instance_type = "t2.micro" 
+  
+  tags{
+     name = "my tree intances"
+  }              
 }
